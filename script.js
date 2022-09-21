96b9b5b67bd330bfd30865d0b9edf8aef6f7cc0a
Ruso Chanturidze <108417178+r140592uso@users.noreply.github.com>
2022-09-22 02:08:02 +0400

    1

---
+++ script.js
@@ -0,0 +1,67 @@
+// 1.ამოიღეთ user ცვლადის სტუდენტის სტატუსი;
+
+// let user = {
+//     fistname: "giorgi",
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus: "active"
+// }
+// console.log(user.studentstatus)
+
+//2.შექმენით მასივი, და დაბეჭდეთ სათითაოდ თითოეული ელემენტი(for da while ციკლით);
+
+// var colors = ["red","blue","green","yellow","purple"];
+// for(var index=0; index<colors.length; index++ )
+// console.log(colors[index]);
+
+// let loading = 0;
+// while(loading<100) {
+//     console.log("Loading please wait");
+//     loading+=20;
+// }
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+// for(let index = 0; index < numbers.length; index++)
+// if(numbers[index] > 5) {
+//     console.log(numbers[index]);
+// }
+
+
+
+// 3.  შექმენით მასივი, და ამოიღეთ ყველა რიცხვი რომელიც მეტია 5ზე, for ციკლის გამოყენებით.
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+// for (let index = 0; index < numbers.length; index++)
+//   if (numbers[index] > 5) {
+//     console.log(numbers[index]);
+//   }
+
+// 4.
+
+// let user = {
+//     name: "giorgi",
+//     age:  20,
+//     studentstatus: "active"
+// }
+// if  (user.age < 18 & user.studentstatus === "active"){
+//     console.log("Hello");
+// }
+// else{
+// console.log("error")
+// }
+
+// 5.გამოიტანეთ მარტო სრტინგები;
+
+// let array = ["watermelon", "pear", 10, 45, 50, "apple", "ananas"];
+// for (let index = 0; index < array.length; index++)
+//   if (typeof array[index] === "string") {
+//     console.log(array[index]);
+//   }
+
+
+// 6. გამოიტანეთ მხოლოდ დადებითი რიცხვები
+
+let array = [ [2, -3, 5, 10], [25, -24, -11, 100] , [-6, -7, 10] ]
+for(let index = 0; )
+if()
+
